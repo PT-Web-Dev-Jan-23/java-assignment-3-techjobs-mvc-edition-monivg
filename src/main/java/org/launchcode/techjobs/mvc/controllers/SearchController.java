aa8649da9d12e8a13ed727324b08ca9ba9094042
monivg <monicav2013@gmail.com>
2023-05-25 18:52:01 -0500

    completed part 3 & 4 of assignment

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -31,7 +31,7 @@ public class SearchController {
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
         ArrayList <Job> jobs;
-        if (searchTerm.equalsIgnoreCase("all")||searchTerm.equals("null")){
+        if (searchTerm.toLowerCase().equals("all")||searchTerm.equals("null")){
             jobs = JobData.findAll();
         } else{
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
